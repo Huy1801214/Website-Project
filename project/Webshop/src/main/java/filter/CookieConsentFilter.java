f676d615e546ad373c9b6f87189790ec845e67c2
Anh Huy <ttahuy1801@gmail.com>
2025-01-13 11:23:51 +0700

    cuối cùng cũng làm đúng cookie

---
+++ project/Webshop/src/main/java/filter/CookieConsentFilter.java
@@ -0,0 +1,116 @@
+//package filter;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.annotation.WebFilter;
+//import jakarta.servlet.http.*;
+//
+//import java.io.IOException;
+//
+//@WebFilter("/*")
+//public class CookieConsentFilter implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+//        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+//        boolean useCookie = false;
+//        Object cookieConsent = null;
+//
+//        if (httpRequest.getSession(false) != null) {
+//            cookieConsent = httpRequest.getSession(false).getAttribute("cookieConsent");
+//        }
+//
+//        if (cookieConsent instanceof Boolean) {
+//            useCookie = ((Boolean) cookieConsent);
+//        } else if (httpRequest.getCookies() != null) {
+//            for (Cookie cookie : httpRequest.getCookies()) {
+//                if (cookie.getName().equals("cookieT")) {
+//                    if (cookie.getValue().equals("true")) {
+//                        useCookie = true;
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+//
+//        if (useCookie) {
+//            filterChain.doFilter(httpRequest, httpResponse);
+//        } else {
+//            HttpServletRequest newRequest = new NoSessionHttpRequest(httpRequest);
+//            filterChain.doFilter(newRequest, httpResponse);
+//        }
+//    }
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//    }
+//
+//    private static class NoSessionHttpRequest extends HttpServletRequestWrapper {
+//        public NoSessionHttpRequest(HttpServletRequest request) {
+//            super(request);
+//        }
+//
+//        @Override
+//        public HttpSession getSession(boolean create) {
+//            return null;
+//        }
+//
+//        @Override
+//        public HttpSession getSession() {
+//            return null;
+//        }
+//    }
+//
+//}
+package filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CookieConsentFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        HttpSession session = httpRequest.getSession(true); // luôn tạo session
+        boolean useCookie = false;
+        Object cookieConsent = session.getAttribute("cookieConsent");
+
+        if (cookieConsent instanceof Boolean){
+            useCookie = (Boolean) cookieConsent;
+        }
+        else if (httpRequest.getCookies() != null) {
+            for (Cookie cookie : httpRequest.getCookies()) {
+                if (cookie.getName().equals("cookieT")) {
+                    if (cookie.getValue().equals("true")) {
+                        useCookie = true;
+                    }
+                    break;
+                }
+            }
+        }
+        httpRequest.setAttribute("useCookie", useCookie);
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
--- project/Webshop/src/main/java/filter/URLRewritingFilter.java
+++ project/Webshop/src/main/java/filter/URLRewritingFilter.java
@@ -1,9 +1,88 @@
+//package filter;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.annotation.WebFilter;
+//import jakarta.servlet.http.*;
+//
+//import java.io.IOException;
+//
+//@WebFilter("/*")
+//public class URLRewritingFilter implements Filter {
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+//        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+//
+//        HttpSession session = httpServletRequest.getSession(false);
+//
+//        boolean hasCookie = hasCookie(httpServletRequest);
+//        if (!hasCookie && session != null) {
+//            HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(httpServletResponse) {
+//                @Override
+//                public String encodeURL(String url) {
+//                    HttpSession session = httpServletRequest.getSession(false);
+//                    if (session != null) {
+//                        String sessionId = session.getId();
+//                        return URLRewritingUtil.encodeURLWithSession(url, sessionId);
+//                    }
+//                    return super.encodeURL(url);
+//                }
+//
+//                @Override
+//                public String encodeRedirectURL(String url) {
+//                    HttpSession session = httpServletRequest.getSession(false);
+//                    if (session != null) {
+//                        String sessionId = session.getId();
+//                        return URLRewritingUtil.encodeURLWithSession(url, sessionId);
+//                    }
+//                    return super.encodeRedirectURL(url);
+//                }
+//            };
+//            System.out.println("dung url");
+//            filterChain.doFilter(servletRequest, responseWrapper);
+//        } else {
+//            //Nếu cookie hoạt động thì kiểm tra xem url có session id không, nếu có thì xoá đi
+//            System.out.println("dung cookie");
+//            String requestURL = httpServletRequest.getRequestURL().toString();
+//
+//            int semicolonIndex = requestURL.indexOf(';');
+//            if (semicolonIndex > -1) {
+//                String newURL = requestURL.substring(0, semicolonIndex);
+//                httpServletResponse.sendRedirect(newURL);
+//                return;
+//            }
+//            filterChain.doFilter(servletRequest, servletResponse);
+//        }
+//    }
+//
+//    private boolean hasCookie(HttpServletRequest httpServletRequest) {
+//        Cookie[] cookies = httpServletRequest.getCookies();
+//        if (cookies != null) {
+//            for (Cookie cookie : cookies) {
+//                if ("JSESSIONID".equals(cookie.getName())) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//    }
+//}
+
 package filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.*;
-
 import java.io.IOException;
 
 @WebFilter("/*")
@@ -17,11 +96,47 @@ public class URLRewritingFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-
         HttpSession session = httpServletRequest.getSession(false);
 
-        boolean hasCookie = hasCookie(httpServletRequest);
-        if (!hasCookie && session != null) {
+
+        boolean useCookie = false;
+        Object cookieConsent = null;
+        if(httpServletRequest.getSession(false)!= null) {
+            cookieConsent = httpServletRequest.getSession(false).getAttribute("cookieConsent");
+        }
+
+        if (cookieConsent instanceof Boolean){
+            useCookie = (Boolean) cookieConsent;
+        }
+
+        else if (httpServletRequest.getCookies() != null) {
+            for (Cookie cookie : httpServletRequest.getCookies()) {
+                if (cookie.getName().equals("cookieT")) {
+                    if (cookie.getValue().equals("true")) {
+                        useCookie = true;
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (useCookie) {
+            //Nếu cookie hoạt động thì kiểm tra xem url có session id không, nếu có thì xoá đi
+            System.out.println("dung cookie");
+            String requestURL = httpServletRequest.getRequestURL().toString();
+
+            int semicolonIndex = requestURL.indexOf(';');
+            if (semicolonIndex > -1) {
+                String newURL = requestURL.substring(0, semicolonIndex);
+                httpServletResponse.sendRedirect(newURL);
+                return;
+            }
+
+            filterChain.doFilter(servletRequest, servletResponse);
+
+        }  else {
+
+            //Nếu không có cookie hoặc cookie bị vô hiệu hóa thì wrap response để thêm session id vào link
             HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(httpServletResponse) {
                 @Override
                 public String encodeURL(String url) {
@@ -45,22 +160,12 @@ public class URLRewritingFilter implements Filter {
             };
             System.out.println("dung url");
             filterChain.doFilter(servletRequest, responseWrapper);
-        } else {
-            //Nếu cookie hoạt động thì kiểm tra xem url có session id không, nếu có thì xoá đi
-            System.out.println("dung cookie");
-            String requestURL = httpServletRequest.getRequestURL().toString();
-
-            int semicolonIndex = requestURL.indexOf(';');
-            if (semicolonIndex > -1) {
-                String newURL = requestURL.substring(0, semicolonIndex);
-                httpServletResponse.sendRedirect(newURL);
-                return;
-            }
-            filterChain.doFilter(servletRequest, servletResponse);
         }
+
     }
 
-    private boolean hasCookie(HttpServletRequest httpServletRequest) {
+
+    private boolean hasSessionCookie(HttpServletRequest httpServletRequest){
         Cookie[] cookies = httpServletRequest.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
@@ -71,7 +176,6 @@ public class URLRewritingFilter implements Filter {
         }
         return false;
     }
-
     @Override
     public void destroy() {
         Filter.super.destroy();
